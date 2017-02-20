07ff3e45a038ff35b08b26f38802f2ee18d85db5
SIDNEYRDC <sydney_rdc@hotmail.com>
2017-02-20 19:06:36 -0300

    second order MPC implemented

--- julia/control.jl
+++ julia/control.jl
@@ -2,7 +2,7 @@
  = Control Module to Topology Control Algorithm in Julia
  =
  = Maintainer: Sidney Carvalho - sydney.rdc@gmail.com
- = Last Change: 2016 Set 25 02:26:56
+ = Last Change: 2017 Fev 20 19:00:52
  = Info: This file contains the motion control algorithms used in the topology
  = control algorithm.
  =============================================================================#
@@ -13,7 +13,7 @@ using Convex    # cvx interface to solve convex problems
 using Gurobi    # to use Gurobi as optimization solver
 
 # public functions
-export hl_motion_control
+export #=mpc_1st_order, =#mpc_2nd_order
 
 #
 #=
@@ -26,7 +26,7 @@ export hl_motion_control
  = array to the neighborhood of i, h is the control step time, p is the
  = prediction horizon to the MPC and gamma is a weight array to MPC.
  =#
-function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, r_cov, dt, p, gamma, phi, RSSI_SENS, vxl, val)
+#=function mpc_1st_order(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, r_cov, dk, p, gamma, phi, RSSI_SENS, vxl, val)
     # auxiliary matrices definition
     Gixy = zeros(p, p)
     Githeta = zeros(p, p)
@@ -47,8 +47,9 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
     ci = [sum(x[[i; N1], 1])/(length(N1) + 1); sum(x[[i; N1], 2])/(length(N1) + 1)]
 
     # calculate the desired angle theta
-    dx = x[i, 1] - ci[1]
-    dx != 0 ? dtheta = atan((x[i, 2] - ci[2])/dx) : dtheta = 0
+    #=dx = x[i, 1] - ci[1]=#
+    #=dx != 0 ? dtheta = atan((x[i, 2] - ci[2])/dx) : dtheta = 0=#
+    dtheta = 3pi/2;
 
     for j in N2
         # auxiliary matrices definition
@@ -69,11 +70,11 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
         psi_ij = Ai[j] + phi_ij
 
         # fill auxiliary matrices
-        Gixy = Gixy + T'*dt*dt*psi_ij*gamma[1]*T + phi_ij*gamma[2] + (psi_ij - Hi[j])*gamma[3] + T'*dt*dt*psi_ij*gamma[7]*T
-        Githeta = Githeta + T'*dt*dt*psi_ij*gamma[1]*T
-        fix = fix + (Xi[:, 1] - Xd[:, 1])'*dt*psi_ij*gamma[1]*T + Vj[:, 1]'*phi_ij*gamma[2] - Vj[:, 1]'*(psi_ij - Hi[j])*gamma[3] - Sij'*dt*psi_ij*gamma[7]*T
-        fiy = fiy + (Xi[:, 2] - Xd[:, 2])'*dt*psi_ij*gamma[1]*T + Vj[:, 2]'*phi_ij*gamma[2] - Vj[:, 2]'*(psi_ij - Hi[j])*gamma[3] - Sij'*dt*psi_ij*gamma[7]*T
-        fitheta = fitheta + (Xi[:, 3] - Xd[:, 3])'*dt*psi_ij*gamma[1]*T
+        Gixy = Gixy + T'*dk*dk*psi_ij*gamma[1]/r_com[j]*T + phi_ij*gamma[2]/vxl[2] + (psi_ij - Hi[j])*gamma[3]/vxl[2] + T'*dk*dk*psi_ij*gamma[7]*T
+        Githeta = Githeta + T'*dk*dk*psi_ij*gamma[1]*T
+        fix = fix + (Xi[:, 1] - Xd[:, 1])'*dk*psi_ij*gamma[1]/r_com[j]*T + Vj[:, 1]'*phi_ij*gamma[2]/vxl[2] - Vj[:, 1]'*(psi_ij - Hi[j])*gamma[3]/vxl[2] - Sij'*dk*psi_ij*gamma[7]*T
+        fiy = fiy + (Xi[:, 2] - Xd[:, 2])'*dk*psi_ij*gamma[1]/r_com[j]*T + Vj[:, 2]'*phi_ij*gamma[2]/vxl[2] - Vj[:, 2]'*(psi_ij - Hi[j])*gamma[3]/vxl[2] - Sij'*dk*psi_ij*gamma[7]*T
+        fitheta = fitheta + (Xi[:, 3] - Xd[:, 3])'*dk*psi_ij*gamma[1]*T
     end
 
     # fill the auxiliary matrices for the references
@@ -83,18 +84,20 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
             Xr = repmat(x[r, :]', p, 1)
             Vr = repmat(v[r, :]', p, 1)
 
-            Gixy = Gixy + T'*dt*dt*Ai[r]*gamma[4]*T + Ai[r]*gamma[5]
-            fix = fix + (Xi[:, 1] - Xr[:, 1])'*dt*Ai[r]*gamma[4]*T - Vr[:, 1]'*Ai[r]*gamma[5]
-            fiy = fiy + (Xi[:, 2] - Xr[:, 2])'*dt*Ai[r]*gamma[4]*T - Vr[:, 2]'*Ai[r]*gamma[5]
+            Gixy = Gixy + T'*dk*dk*Ai[r]*gamma[4]*T + Ai[r]*gamma[5]
+            fix = fix + (Xi[:, 1] - Xr[:, 1])'*dk*Ai[r]*gamma[4]*T - Vr[:, 1]'*Ai[r]*gamma[5]
+            fiy = fiy + (Xi[:, 2] - Xr[:, 2])'*dk*Ai[r]*gamma[4]*T - Vr[:, 2]'*Ai[r]*gamma[5]
         end
     end
 
     # fill auxiliary matrices
-    Gixy = Gixy + TI'*gamma[6]*TI
+    Gixy = Gixy + TI'*gamma[6]/vxl[2]*TI
     Githeta = Githeta + TI'*gamma[6]*TI
-    fix = fix - Vi0[:, 1]'*gamma[6]*TI
-    fiy = fiy - Vi0[:, 2]'*gamma[6]*TI
+    fix = fix - Vi0[:, 1]'*gamma[6]/vxl[2]*TI
+    fiy = fiy - Vi0[:, 2]'*gamma[6]/vxl[2]*TI
     fitheta = fitheta - Vi0[:, 3]'*gamma[6]*TI
+
+    #=println([Gixy zeros(p, p) zeros(p, p); zeros(p, p) Gixy zeros(p, p); zeros(p, p) zeros(p, p) Githeta])=#
 
     # declare optimization problem variables
     Ux = Convex.Variable(p)
@@ -103,7 +106,7 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
 
     # objective function
     problem = minimize(
-        (1/2)*quadform([Ux; Uy; Utheta], [Gixy zeros(p, p) zeros(p, p); zeros(p, p) Gixy zeros(p, p); zeros(p, p) zeros(p, p) Githeta]) + [fix fiy fitheta]*[Ux; Uy; Utheta],
+        (1/2)*Convex.quadform([Ux; Uy; Utheta], [Gixy zeros(p, p) zeros(p, p); zeros(p, p) Gixy zeros(p, p); zeros(p, p) zeros(p, p) Githeta]) + [fix fiy fitheta]*[Ux; Uy; Utheta],
         # saturation constraints
         Ux >= fill(vxl[1], p),
         Uy >= fill(vxl[1], p),
@@ -115,7 +118,7 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
 
     # maximum distance between neighbours constraint
     for j in N1
-        problem.constraints += norm(x[i, 1 : 2] + [Ux[1] Uy[1]]'*dt - x[j, 1 : 2]) <= r_com[j]
+        problem.constraints += norm(x[i, 1 : 2] + [Ux[1] Uy[1]]'*dk - x[j, 1 : 2]) <= r_com[j]
     end
 
     # solve the optimization problem
@@ -126,6 +129,114 @@ function hl_motion_control(i, Ai, Hi, Di, Si, T, TI, n_bot, n_ref, x, v, r_com, 
         return [Ux.value[1] Uy.value[1] Utheta.value[1]], ci
     else
         return [0.0 0.0 0.0], ci
+    end
+end
+
+end =#
+
+#
+#=
+ =
+ =#
+
+#= second order mpc control =#
+function mpc_2nd_order(i, n, dk, p, x, v, u, Ai, Hi, Di, Si, T, TI, T2, P, r_com, r_cov, gamma, u_min, u_max)
+    # auxiliary matrices definition
+    Hixy = zeros(p, p)              # Hessian matrix to x and y coordinates
+    Hitheta = zeros(p, p)           # Hessian matrix to yaw angle
+    gix = zeros(1, p)               # gradient matrix to x coordinate
+    giy = zeros(1, p)               # gradient matrix to y coordinate
+    githeta = zeros(1, p)           # gradient matrix to yaw angle
+
+    Xi = repmat(x[i, :]', p, 1)     # NOTE: after julia upgrade to 0.5 version, a subvector of a vector is ALWAYS a column vector.
+    Vi = repmat(v[i, :]', p, 1)
+    Ui0 = zeros(p, 3)
+    Ui0[1, :] = u[i, :]
+
+    # limits to velocity variation
+    const v_max = u_max[1]*dk
+
+    # get the 1-hop neighbours of i
+    N1 = find(Ai[1 : n])
+
+    # get the real and virtual neighbours of i (1-hop + 2-hop neighbours)
+    N2 = find(max(Ai[1 : n], Hi))
+
+    # get desired angle
+    dtheta = 0
+
+    for j in N2
+        # auxiliary matrices definition
+        Vj = repmat(v[j, :]', p, 1)
+
+        # calculate the desired position to x and y coordinates
+        dxy = (x[i, 1 : 2] - x[j, 1 : 2])*(r_cov[i] + r_cov[j])/Di[j]+ x[j, 1 : 2]
+
+        # auxiliary matrix to desired position
+        Xij = repmat([dxy' dtheta], p, 1)
+
+        # term activation
+        phi_ij = (1 - Ai[j])*Hi[j]          # activated for virtual neighbors
+        psi_ij = Ai[j] + phi_ij             # activated for all neighbors (physical + virtual)
+
+        # fill Hessian matrices for each neighbor
+        Hixy += T2'*dk^4*gamma[1]/r_com[j]*psi_ij*T2        # penalize position error
+        Hixy += T'*dk^2*gamma[2]/2*v_max*phi_ij*T           # penalize same direction velocities (on virtual neighbors)
+        Hixy += T'*dk^2*gamma[3]/v_max*(psi_ij - Hi[j])*T   # penalize different direction velocities (on physical neighbors)
+        Hitheta += T2'*dk^4*gamma[1]*psi_ij*T2
+
+        # fill gradient matrices for each neighbor
+        gix += (Xi[:, 1] - Xij[:, 1])'*dk^2*gamma[1]/r_com[j]*psi_ij*T2
+        giy += (Xi[:, 2] - Xij[:, 2])'*dk^2*gamma[1]/r_com[j]*psi_ij*T2
+        githeta += (Xi[:, 3] - Xij[:, 3])'*dk^2*gamma[1]*psi_ij*T2
+        gix += Vi[:, 1]'*P'*dk^3*gamma[1]/r_com[j]*psi_ij*T2
+        giy += Vi[:, 2]'*P'*dk^3*gamma[1]/r_com[j]*psi_ij*T2
+        githeta += Vi[:, 3]'*P'*dk^3*gamma[1]*psi_ij*T2
+        gix += (Vi[:, 1] + Vj[:, 1])'*dk*gamma[2]/2*v_max*phi_ij*T
+        giy += (Vi[:, 2] + Vj[:, 2])'*dk*gamma[2]/2*v_max*phi_ij*T
+        gix += (Vi[:, 1] - Vj[:, 1])'*dk*gamma[3]/v_max*(psi_ij - Hi[j])*T
+        giy += (Vi[:, 2] - Vj[:, 2])'*dk*gamma[3]/v_max*(psi_ij - Hi[j])*T
+    end
+
+    # fill Hessian matrices
+    Hixy += TI'*gamma[4]/u_max[1]*TI
+    Hitheta += TI'*gamma[4]*TI
+
+    # fill gradient matrices
+    gix -= Ui0[:, 1]'*gamma[4]/u_max[1]*TI
+    giy -= Ui0[:, 2]'*gamma[4]/u_max[1]*TI
+    githeta -= Ui0[:, 3]'*gamma[4]*TI
+
+    # declare optimization problem variables
+    Ux = Convex.Variable(p)
+    Uy = Convex.Variable(p)
+    Utheta = Convex.Variable(p)
+
+    # objective function
+    problem = minimize(
+        (1/2)*quadform([Ux; Uy; Utheta], [Hixy zeros(p, p) zeros(p, p); zeros(p, p) Hixy zeros(p, p); zeros(p, p) zeros(p, p) Hitheta]) + [gix giy githeta]*[Ux; Uy; Utheta],
+        # saturation constraints
+        Ux >= fill(u_min[1], p),
+        Uy >= fill(u_min[2], p),
+        Utheta >= fill(u_min[3], p),
+        Ux <= fill(u_max[1], p),
+        Uy <= fill(u_max[2], p),
+        Utheta <= fill(u_max[3], p)
+    )
+
+    # maximum distance between neighbours constraint
+    for j in N1
+        problem.constraints += norm(x[i, 1 : 2] + v[i, 1 : 2]*dk + [Ux[1] Uy[1]]'*dk^2 - x[j, 1 : 2]) <= r_com[j]
+    end
+
+    # solve the optimization problem
+    solve!(problem, GurobiSolver(OutputFlag = 0))
+
+    # verify if the problem was solved
+    if problem.status == :Optimal
+        return [Ux.value[1] Uy.value[1] Utheta.value[1]]
+    else
+        return [0.0 0.0 0.0]
     end
 end
 
